a5d2cd316f212cfba0ce86c64c5b85513343c934
wr9dg17 <wr9dg17@gmail.com>
2022-08-23 22:01:01 +0400

    Created project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
